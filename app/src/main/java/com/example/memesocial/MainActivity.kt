e1a01e2e987d2fff5ac1e26739ff176b0c377342
Ayush <ayush8402@gmail.com>
2021-08-25 01:50:54 +0530

    meme shows

--- app/src/main/java/com/example/memesocial/MainActivity.kt
+++ app/src/main/java/com/example/memesocial/MainActivity.kt
@@ -2,14 +2,37 @@ package com.example.memesocial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
+    fun loadMeme(){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
 
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val imgurl = response.getString("url")
+                Glide.with(this).load(imgurl).into(findViewById(R.id.memeImageView))
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+            }
+        )
+    }
     fun shareMeme(view: View) {}
     fun nextMeme(view: View) {}
 }
