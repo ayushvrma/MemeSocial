7e060f263985312478b82e6fc2d577cd1652fb12
Ayush <ayush8402@gmail.com>
2021-08-25 01:16:41 +0530

    made basic layout of main screen

--- app/src/main/java/com/example/memesocial/MainActivity.kt
+++ app/src/main/java/com/example/memesocial/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.memesocial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {}
 }
